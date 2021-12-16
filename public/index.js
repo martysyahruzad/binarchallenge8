271b582431c0f774fe35538f119d7d4b60df3673
martysyahruzad <86584071+martysyahruzad@users.noreply.github.com>
2021-12-15 22:29:10 -0600

    Create index.js

---
+++ public/index.js
@@ -0,0 +1,10 @@
+export { default as HomepageMain } from './HomepageMain'
+export { default as HomepageCarousel } from './HomepageCarousel'
+export { default as HomepageFeatures } from './HomepageFeatures'
+export { default as HomepageSysreq } from './HomepageSysreq'
+export { default as HomepageQuotes } from './HomepageQuotes'
+export { default as HomepageSubscribe } from './HomepageSubscribe'
+
+export { default as NavBar } from './NavBar'
+
+export { default as SuitImage } from './RockPaperScissorsImage'
