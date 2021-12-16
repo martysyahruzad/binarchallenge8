b97a2018ae2583dd7429e26c3925e82dde455ad5
martysyahruzad <86584071+martysyahruzad@users.noreply.github.com>
2021-12-15 22:26:50 -0600

    Create db.js

---
+++ db.js
@@ -0,0 +1,9 @@
+import 'tailwindcss/tailwind.css'
+import '../styles/index.css'
+import '../styles/dashboard.css'
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
